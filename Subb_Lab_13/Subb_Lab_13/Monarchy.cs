6fa4ed54b7133340db5c8ff3dd47c03f32cd82dc
Alexey Subbotin <subbotin910@gmail.com>
2018-05-07 08:42:37 +0500

    other classes added

--- Subb_Lab_13/Subb_Lab_13/Monarchy.cs
+++ Subb_Lab_13/Subb_Lab_13/Monarchy.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Subb_Lab12
+namespace Subb_Lab_13
 {
-    // Monarchy class (inherits AbstrState class)
-    class Monarchy : AbstrState
+    // Monarchy class (inherits State class)
+    class Monarchy : State
     {
         // Name of current rulling clan
-        protected string currentRullingClanName;
+        protected string currentRullingClanName; 
 
         // currentRulingClanNam property
         public string CurrentRullingClanName
@@ -26,23 +26,23 @@ namespace Subb_Lab12
         }
 
         // Constructor without parameters
-        public Monarchy() : base()
+        public Monarchy() : base() 
         {
             CurrentRullingClanName = "";
         }
         // Constructor with parameters
         public Monarchy(string Name, string lName, int Pop, int Age, string Cont,
-            string curr) : base(Name, lName, Pop, Age, Cont)
+            string curr):base(Name, lName, Pop, Age, Cont) 
         {
             CurrentRullingClanName = curr;
         }
 
         // Redefinition of the Show method
-        public override void Show()
+        public override void Show() 
         {
             Console.WriteLine(" Monarchical state's name: {0}\n Monarch's name: {1}\n Monarchical state's population: {2}\n " +
                 "Monarchical state's age: {3}\n Continent: {4}\n " +
-                "Current rulling clan: {5}\n",
+                "Current rulling clan: {5}\n", 
                 Name, LeaderName, Population, Age, Continent, CurrentRullingClanName);
         }
     }
