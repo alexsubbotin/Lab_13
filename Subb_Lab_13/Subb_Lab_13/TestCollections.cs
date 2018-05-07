0d9429846d6271236d2b64de866a6f2cb48e95a1
Alexey Subbotin <subbotin910@gmail.com>
2018-05-07 08:53:46 +0500

    TestCollections class added

---
+++ Subb_Lab_13/Subb_Lab_13/TestCollections.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subb_Lab_13
+{
+    class TestCollections
+    {
+        // List<TKey>.
+        List<State> ListState { get; set; }
+
+        // List<string>.
+        List<string> ListString { get; set; }
+
+        // Dictionary<TKey, TValue>.
+        Dictionary<State, Monarchy> DictionaryState { get; set; }
+
+        // Dictionary<string, TValue>.
+        Dictionary<string, Monarchy> DictionaryString { get; set; }
+    }
+}
